b8c3c8b471cb3d8ce4e73156298260abb2024d67
Osmond <Kingozzy24@gmail.com>
2012-12-29 17:08:42 -0800

    sprite class linked

--- ArtScramble/ArtScramble/Sprite.cs
+++ ArtScramble/ArtScramble/Sprite.cs
@@ -14,7 +14,31 @@ namespace ArtScramble
         public Vector2 Position = new Vector2(0, 0);
 
         //The texture object used when drawing the sprite
-        private Texture2D mSpriteTexture;
-
+        private Texture2D SpriteTexture;
+        private Rectangle TitleSafe;
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            SpriteTexture = Content.Load<Texture2D>("ArtScrambleContent/rough_player");
+            TitleSafe = GetTitleSafeArea(.8f);
+        }
+        protected Rectangle GetTitleSafeArea(float percent)
+        {
+            Rectangle retval = new Rectangle(
+                graphics.GraphicsDevice.Viewport.X,
+                graphics.GraphicsDevice.Viewport.Y,
+                graphics.GraphicsDevice.Viewport.Width,
+                graphics.GraphicsDevice.Viewport.Height);
+        }
+        protected override void Draw(GameTime gameTime)
+        {
+            graphics.GraphicsDevice.Clear(Color.CornflowerBlue);
+            spriteBatch.Begin();
+            Vector2 pos = new Vector2(TitleSafe.Left, TitleSafe.Top);
+            spriteBatch.Draw(SpriteTexture, pos, Color.White);
+            spriteBatch.End();
+            base.Draw(gameTime);
+        }
     }
 }
